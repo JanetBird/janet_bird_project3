0d638f09714a2ce6d7639d36c89f1d2dde84f8e2
Your <janetsbird@gmail.com>
2018-11-13 17:39:25 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const Destination = {
+    Nature: [
+        {
+            place: "Lauterbrunnen",
+            people: [couple, alone, family, friends]
+            activityLevel: "alot", 
+        },
+        {
+            place: "Railay Beach",
+            people: [couple, alone, friends],
+            activityLevel: "little",
+        }
+        {
+            place: "Jasper",
+            people: [family, couple],
+            activityLevel: "nothing"
+        }
+    ],
+    City: [
+        {
+            place: "Kyoto",
+            people: [family, alone],
+            activityLevel: "alot", 
+        },
+        {
+            place: "Barcelona",
+            people: [couple, family],
+            activityLevel: "a little"
+        }
+        {
+            place: "New York City",
+            people: [couple, alone],
+            activityLevel: "nothing",
+        }
+    ],
+    Culture: [
+        {
+            place: "Bruges",
+            people: [couple, alone],
+            activityLevel: "nothing",
+        },
+        {
+            place: "Berlin",
+            people: [family, friends, couple],
+            activityLevel: "alot", 
+        }
+        {
+            place: "Stuttgart",
+            people: [family, alone, friends],
+            activityLevel: "little",
+        }
+    ]
+};
